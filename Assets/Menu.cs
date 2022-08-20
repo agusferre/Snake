98a5933c9e6fa9cc7f5e6cdf308c5dba00dd77c9
Agustín Ferré <nitsu@Agustins-MacBook-Pro.local>
2022-08-20 18:20:08 -0300

    first commit

---
+++ Assets/Menu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void playGame(){
+        SceneManager.LoadScene("Game");
+    }
+
+    public void quitGame(){
+        Application.Quit();
+    }
+}
